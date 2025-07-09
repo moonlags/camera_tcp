5e9dab13b954f5c274fa98632a87b82abe72faa1
moonlags <moonlags.me@proton.me>
2025-07-09 17:30:14 +0300

    fix

--- client.go
+++ client.go
@@ -36,21 +36,10 @@ func (c Client) handleConnection() {
 			break
 		}
 
-		photoConfigs := make([]PhotoConfig, 2)
-		buf = buf[len(PASSWORD):n]
-		reader := bytes.NewReader(buf)
+		amount := (n - len(PASSWORD)/int(unsafe.Sizeof(PhotoConfig{})))
+		photoConfigs := make([]PhotoConfig, amount)
+		reader := bytes.NewReader(buf[len(PASSWORD):n])
 
-		// for len(buf) > 0 {
-		// 	var cfg PhotoConfig
-		//
-		// 	if err := binary.Read(reader, binary.BigEndian, &cfg); err != nil {
-		// 		log.Printf("failed to decode binary data %s\n", err)
-		// 		break
-		// 	}
-		//
-		// 	photoConfigs = append(photoConfigs, cfg)
-		// 	buf = buf[unsafe.Sizeof(PhotoConfig{}):]
-		// }
 		if err := binary.Read(reader, binary.BigEndian, photoConfigs); err != nil {
 			log.Printf("failed to decode binary data %s\n", err)
 			break
